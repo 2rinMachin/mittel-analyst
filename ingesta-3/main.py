da341e6d2c1a4728aaf233e5a09d7ecda3a098fa
José Daniel Grayson <josedanielgrayson@proton.me>
2025-10-03 13:54:03 -0500

    feat(ingesta-3): fetch devices table

--- ingesta-3/main.py
+++ ingesta-3/main.py
@@ -1,10 +1,12 @@
 from datetime import datetime
+from mypy_boto3_s3 import S3Client
 import os
 import json
-from typing import Any
 import boto3
 
 from mysql.connector import connect
+from mysql.connector.abstracts import MySQLCursorAbstract
+from mysql.connector.types import RowItemType, RowType
 
 DB_HOST = os.environ["DB_HOST"]
 DB_PORT = os.environ["DB_PORT"]
@@ -15,31 +17,27 @@ DB_DATABASE = os.environ["DB_DATABASE"]
 S3_BUCKET = os.environ["S3_BUCKET"]
 
 
-def serializer(obj: Any):
+def serializer(obj: object):
     if isinstance(obj, datetime):
         return obj.strftime("%Y-%m-%d %H:%M:%S")
 
     raise TypeError(f"Type {type(obj)} is not serializable")
 
 
-def fetch_all_events():
-    with connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        user=DB_USERNAME,
-        password=DB_PASSWORD,
-        database=DB_DATABASE,
-    ) as conn:
-        with conn.cursor(dictionary=True) as cursor:
-            _ = cursor.execute("select * from events")
-            return cursor.fetchall()
+def fetch_all_events(cursor: MySQLCursorAbstract):
+    _ = cursor.execute("select * from events")
+    return cursor.fetchall()
+
+def fetch_all_devices(cursor: MySQLCursorAbstract):
+    _ = cursor.execute("select * from devices")
+    return cursor.fetchall()
 
 
-def upload_json_rows(rows: list[Any], key: str): 
+def upload_json_rows(client: S3Client, rows: list[RowType | dict[str, RowItemType]], key: str): 
     body = "\n".join(json.dumps(r,default=serializer) for r in rows)
+    print(body)
 
-    s3 = boto3.client("s3")
-    s3.put_object(
+    _ = client.put_object(
         Bucket=S3_BUCKET,
         Key=key,
         Body=body.encode("utf-8"),
@@ -49,10 +47,23 @@ def upload_json_rows(rows: list[Any], key: str):
 
 def main():
     print("Fetching events...")
-    events = fetch_all_events()
+
+    with connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_USERNAME,
+        password=DB_PASSWORD,
+        database=DB_DATABASE,
+    ) as conn:
+        with conn.cursor(dictionary=True) as cursor:
+            events = fetch_all_events(cursor)
+            devices = fetch_all_devices(cursor)
 
     print("Uploading events...")
-    upload_json_rows(events, "engagement/events/events.json")
+
+    client: S3Client = boto3.client("s3")  # pyright: ignore[reportUnknownMemberType]
+    upload_json_rows(client, events, "engagement/events/events.json")
+    upload_json_rows(client, devices, "engagement/devices/devices.json")
 
     print("Data uploaded successfully.")
 
